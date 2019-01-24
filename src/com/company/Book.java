198b193ff9d7bac302efff483eb4d376321f38b2
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-24 14:32:49 -0500

    created setters and getters for book class

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -5,4 +5,40 @@ public class Book {
     private String author;
     private String genre;
     private int numberOfPages;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public void setNumberOfPages(int numberOfPages){
+        this.numberOfPages = numberOfPages;
+    }
+
+    public int getNumberOfPages(){
+        return numberOfPages;
+    }
+
+
+
+
 }
