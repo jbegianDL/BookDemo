f7e75a79840eb8f66fcac066ffd752bcb87cca7c
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-28 15:56:30 -0500

    forgot to commit for a long while... lots of methods got created

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -2,10 +2,19 @@ package com.company;
 import java.util.Scanner;
 
 public class Library {
-    private int shelfSpot;
     private Book [] shelf = new Book[3];
 
+
     public void addBook(){
+        if (getNumberOfBooks() == shelf.length){
+            System.out.println("No room");
+        }else{
+            shelf[getNumberOfBooks()] = makeBook();
+        }
+
+    }
+
+    public Book makeBook(){
         Book newBook = new Book();
         System.out.println("Provide Title");
         Scanner input = new Scanner(System.in);
@@ -16,15 +25,67 @@ public class Library {
         newBook.setGenre(input.nextLine());
         System.out.println("Provide Number of Pages");
         newBook.setNumberOfPages(input.nextInt());
+
+        return newBook;
     }
 
-    public int getShelfSpot(){
+    public int getNumberOfBooks(){
+        int numberOfBooks = 0;
         for(int i = 0; i < shelf.length; i++){
             if(shelf[i] == null){
-                shelfSpot = i;
+                numberOfBooks = i;
                 break;
+            }else if (i == (shelf.length - 1)){
+                numberOfBooks = shelf.length;
             }
         }
-        return shelfSpot;
+        return numberOfBooks;
+    }
+
+
+
+    public void printLibrary(){
+        for(int i = 0; i < shelf.length; i++){
+            if(shelf[i] != null){
+                System.out.println((i + 1) + ": Title: " + shelf[i].getTitle() + " -- Author: " + shelf[i].getAuthor() + " -- Genre: " + shelf[i].getGenre() + " -- Pages: " + shelf[i].getNumberOfPages());
+            }else {
+                System.out.println((i + 1) + ": ");
+            }
+
+        }
+    }
+
+    public void searchLibrary(){
+        String wantedBook;
+        System.out.println("What is the title of the book you're looking for?");
+        Scanner input = new Scanner(System.in);
+        wantedBook = input.nextLine();
+        for(int i = 0; i < shelf.length; i++){
+            if(shelf[i] != null && wantedBook.equals(shelf[i].getTitle())){
+                System.out.println("We've got it!");
+                break;
+            } else {
+                System.out.println("We don't have the book you're looking for...");
+            }
+        }
+    }
+
+    public void deleteBook(){
+        String notWantedBook;
+        printLibrary();
+        System.out.println("What book would you like to delete? \n" +
+                "Please enter the title of the book: ");
+        Scanner input = new Scanner(System.in);
+        notWantedBook = input.nextLine();
+        for(int i = 0; i < shelf.length; i++){
+            if(shelf[i] != null && notWantedBook.equals(shelf[i].getTitle())){
+                shelf[i] = null;
+                break;
+            } else {
+                System.out.println("We don't have the book you're looking for...");
+            }
+        }
+
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,52 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Library myLibrary = new Library();
+        boolean repeat = true;
+        Scanner input = new Scanner(System.in);
+        String lineBreak = "\n";
+        String prompt = "Choose what you'd like to do: " + lineBreak + "1 - Number of Books in Library" +
+                lineBreak + "2 - Add a Book" + lineBreak + "3 - Print Library" + lineBreak + "4 - Search Library" +
+                lineBreak + "5 - Delete a Book" + lineBreak + "6 - Exit";
 
-        System.out.println("Open space on shelf is at: " + myLibrary.getShelfSpot());
-        myLibrary.addBook();
+//        myLibrary.getNumberOfBooks(); <-- Prints number of books
+//        myLibrary.addBook(); <-- adds a book
+//        myLibrary.printLibrary(); <-- prints library
+
+        do{
+            System.out.println(prompt);
+            int usrChoice = input.nextInt();
+
+            switch (usrChoice){
+                case 1:
+                    System.out.println("Number of books: " + myLibrary.getNumberOfBooks());
+                    break;
+                case 2:
+                    myLibrary.addBook();
+                    break;
+                case 3:
+                    myLibrary.printLibrary();
+                    break;
+                case 4:
+                    myLibrary.searchLibrary();
+                    break;
+                case 5:
+                    myLibrary.deleteBook();
+                    break;
+                case 6:
+                    System.out.println("Goodbye");
+                    repeat = false;
+                    break;
+                default:
+                    System.out.println("Error - Incorrect input" + lineBreak + "Please try again...");
+            }
+
+        }while(repeat);
+
+
+
     }
 }
